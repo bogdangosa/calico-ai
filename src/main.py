9b106a01cbe1db0ce75e7e1c1b02587096bde701
bogdan <bogdan.gosa@gmail.com>
2026-06-09 17:26:04 +0300

    cors fix

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from src.api.routes import router as game_router
 from src.api.sockets import router as socket_router
 from src.repository.database.connection import dispose_engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(game_router, prefix="/game", tags=["game"])
 app.include_router(socket_router, tags=["sockets"])
 
